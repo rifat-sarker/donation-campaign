2e054737ffc9a548dcb862e3fb6845bd6a641415
unknown <rifat-sarker>
2023-11-10 21:58:43 +0600

    first commit

---
+++ src/Components/Root/Root.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const Root = () => {
+    return (
+        <div>
+            <Header></Header>
+            
+        </div>
+    );
+};
+
+export default Root;
